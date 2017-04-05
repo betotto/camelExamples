58ed6c40c68f8f0c9812f1617ca2e35690e6e5d1
Roberto Gonzalez <gonzalez@allianceit.com>
2017-04-05 09:54:16 -0700

    error

--- src/main/java/mx/com/betotto/graphics/histogramReport/Route.java
+++ src/main/java/mx/com/betotto/graphics/histogramReport/Route.java
@@ -7,7 +7,6 @@ package mx.com.betotto.graphics.histogramReport;
 
 import javax.ws.rs.core.MediaType;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.cdi.ContextName;
 
 /**
  *
@@ -20,8 +19,9 @@ public class Route extends RouteBuilder{
         rest()
             .post("histogramReport")
                 .produces(MediaType.APPLICATION_JSON)
-                .to("sql:select REQUEST_TIME from BI_AUDIT_API?dataSource=briefingiqDataSource")
                 .route()
+                .to("sql:select REQUEST_TIME from BI_AUDIT_API?dataSource=briefingiqDataSource")
+                .log("${body}")
                 .transform().body();
     }
 }
